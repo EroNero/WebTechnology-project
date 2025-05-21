5c57025aeb972c1e09c554ff98f853a91e056d39
EroNero <eren54k@hotmail.com>
2025-05-21 19:07:41 +0300

    login
    login sessionvs

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+$valid_username = 'b241210003@sakarya.edu.tr';
+$valid_password = 'b241210003';
+if ($username === $valid_username && $password === $valid_password) {
+    session_start();
+    $_SESSION['username'] = $username;
+    header('Location: index.html');
+    exit();
+} else {
+    echo "Kullanıcı adı veya şifre hatalı!";
+}
+?>
